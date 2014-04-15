9b63fc1f866b41be2c47fc4ab21c313a126e2ed6
Chase Olivierigit config --global user.name Chase <chase.olivieri@gmail.com>
2014-04-14 21:09:36 -0400

    update to show brand images and product scaffold

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
 
+  def index
+    @products = Product.all
+  end
+
   def show
     @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def edit
   end
 
   def submit
@@ -9,5 +20,50 @@ class ProductsController < ApplicationController
     submission = product.submissions.create(brand_id: product.brand_id, user_id: current_user.id)
     redirect_to submission_path(submission)
   end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @product }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @product.destroy
+    respond_to do |format|
+      format.html { redirect_to products_url }
+      format.json { head :no_content }
+    end
+  end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params.require(:product).permit(:name, :brand_id, :collection_year, :image)
+    end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414151532) do
+ActiveRecord::Schema.define(version: 20140415000027) do
 
   create_table "brands", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "products", force: true do |t|
