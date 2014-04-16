7d96ddecb03530ccbbab1cc3959dc27df9098ca2
Chase Olivierigit config --global user.name Chase <chase.olivieri@gmail.com>
2014-04-16 17:53:25 -0400

    add submit action to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -30,6 +30,12 @@ class ProductsController < ApplicationController
     end
   end
 
+  def submit
+    product = Product.find(params[:id])
+    submission = product.submissions.create(brand_id: product.brand_id, user_id: current_user.id)
+    redirect_to submission_path(submission)
+  end
+
   def update
     respond_to do |format|
       if @product.update(product_params)
