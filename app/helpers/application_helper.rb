2c787366ab90405aa91b3890db73d3db55f8d4d3
Chase Olivierigit config --global user.name Chase <chase.olivieri@gmail.com>
2014-04-22 11:56:34 -0400

    heroku mods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-
-  def typekit_include_tag apikey
-  javascript_include_tag("//use.typekit.com/euf0zmd.js") +
-  javascript_tag("try{Typekit.load()}catch(e){}")
-end
-
 end
