dfe90449e1ce5e09e2bf1988eb5f25541d5d7e14
Chase Olivierigit config --global user.name Chase <chase.olivieri@gmail.com>
2014-04-23 14:49:38 -0400

    oh ship page

--- config/routes.rb
+++ config/routes.rb
@@ -26,5 +26,7 @@ Recycle::Application.routes.draw do
   get "labels" => "pages#labels"
   get "consumers" => "pages#consumers"
   get "manufacturers" => "pages#manufacturers"
+
+  get "ohship" => "pages#ohship"
   
 end
