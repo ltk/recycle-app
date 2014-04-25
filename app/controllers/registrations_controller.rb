26a644e4798b6bb72d4c94fbcc081b4c25bb6c89
Chase Olivierigit config --global user.name Chase <chase.olivieri@gmail.com>
2014-04-25 15:02:44 -0400

    segment changes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
     super
     if resource.persisted?
       Analytics.identify(
-        user_id: 'current_user.id',
-        traits: { email: 'current_user.email', zipcode: 'current_user.zipcode' })
+        user_id: current_user.id,
+        traits: { email: current_user.email, zipcode: current_user.zipcode })
     end
   end
 end
